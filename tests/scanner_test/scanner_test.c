958d6b90d96c667e5013b1cdfbb395638d236cfc
NikitaKotvitskiy <107050567+NikitaKotvitskiy@users.noreply.github.com>
2022-10-25 00:45:18 +0200

    Add files via upload

---
+++ tests/scanner_test/scanner_test.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "scanner.h"
+#include "ifj_string.h"
+
+void printToken(token_t tok)
+{
+    switch (tok.type) {
+        case id_tok:
+            printf("[id: %s]\n", stringRead(&(tok.textData)));
+            break;
+        case var_tok:
+            printf("[var: %s]\n", stringRead(&(tok.textData)));
+            break;
+        case int_tok:
+            printf("[int: %d]\n", tok.numericData.ivalue);
+            break;
+        case float_tok:
+            printf("[float: %f]\n", tok.numericData.fvalue);
+            break;
+        case string_tok:
+            printf("[string: %s]\n", stringRead(&(tok.textData)));
+            break;
+        case brack_tok:
+            printf("[brack: %d]\n", tok.numericData.ivalue);
+            break;
+        case c_brack_tok:
+            printf("[cbrack: %d]\n", tok.numericData.ivalue);
+            break;
+        case oper_tok:
+            printf("[oper: %s]\n", stringRead(&(tok.textData)));
+            break;
+        case comma_tok:
+            printf("[comma: ]\n");
+            break;
+        case semicolon_tok:
+            printf("[semicolon: ]\n");
+            break;
+        case colon_tok:
+            printf("[colon: ]\n");
+            break;
+        case assig_tok:
+            printf("[assig: ]\n");
+            break;
+        case eof_tok:
+            printf("[eof: ]\n");
+            break;
+        default:
+            break;
+    }
+}
+
+int main()
+{
+    scanner_t scan;
+    scannerInit(&scan);
+    TokenType lastTok = init_tok;
+    while (lastTok != eof_tok)
+    {
+        token_t * tok = readToken(&scan);
+        lastTok = tok->type;
+        printToken(*tok);
+    }
+    return 0;
+}
