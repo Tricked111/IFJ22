0fb33e3c2c24d1d869b91c454c102ff5f17bad73
NikitaKotvitskiy <107050567+NikitaKotvitskiy@users.noreply.github.com>
2022-10-26 21:13:29 +0200

    Fixed === and !== operators processing

--- src/ifj_string.h
+++ src/ifj_string.h
@@ -1,42 +1,100 @@
 
 /******************************************************************************
  *                                  IFJ22
- *                              ifj_string.h
+ *                                scanner.h
  * 
  *      Authors: Nikita Kotvitskiy (xkotvi01)
- *      Purpose: Declaration of string structure and operations on them
+ *      Purpose: Declaration of scanner and token structures and operations on them
  * 
  *                        Last change: 24. 10. 2022
  *****************************************************************************/
 
-#ifndef IFJ_STRING_H
-#define IFJ_STRING_H
+#ifndef SCANNER_H
+#define SCANNER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "ifj_string.h"
+#include <ctype.h>
+#include <stdbool.h>
 
-#define DEFAULT_STRING_LENGTH 16    //The number of byte allocated for string during initiation.
+//States of scanner FSM.
+typedef enum {
+    Start,
+    Num,
+    FloatDot,
+    FloatE,
+    Float,
+    StringStart,
+    EsqSeq,
+    StringEnd,
+    VarStart,
+    VarName,
+    Identifier,
+    Colon,
+    Semicolon,
+    Comma,
+    Brack,
+    CBrack,
+    Operator,
+    Assig,
+    OperInter1,
+    OperInter2,
+    OperInter3,
+    Slash,
+    LineCom,
+    BlockCom,
+    BlockComEnd,
+    Error,
+    EndOfTok,
+    EndOfFile
+} FSMSTate;
 
-typedef struct string {
-    char * str;
-    size_t len;
-    size_t allocated;   //Number of currently allocated bytes.
-} string_t;
+//Types of tokens.
+typedef enum {
+    init_tok,
+    id_tok,
+    var_tok,
+    int_tok,
+    float_tok, 
+    string_tok,
+    brack_tok,
+    c_brack_tok,
+    oper_tok,
+    comma_tok,
+    semicolon_tok,
+    colon_tok,
+    assig_tok,
+    eof_tok
+} TokenType;
 
-//Initiates string on str address.
-void stringInit(string_t * str);
-//Free string on str address.
-void stringFree(string_t * str);
-//Appends new symbol newChar to the end of string on str address.
-void stringAppend(string_t * str, char newChar);
-//Erases a string on str address.
-void stringClear(string_t * str);
-//Copies string on strSource address to string on strDest address.
-void stringCopy(string_t * strDest, string_t * strSource);
-//Returns pointer to the first char of string in str address.
-char * stringRead(string_t * str);
-//Returns length of string on str address.
-int stringLength(string_t * str);
+
+typedef struct Token {
+    TokenType type;
+    union TokenData {
+        int ivalue;
+        float fvalue; 
+    } numericData;
+    string_t textData;
+} token_t;
+
+typedef struct Scanner {
+    int charToProcess;             //Char which is currently processed.
+    string_t tokenString;
+    TokenType state;
+    bool doNotWriteCurrentChar;     //If true, charToProcess will not be appended at the end of tokenString.
+                                    //It is useful if we read the first symbol of next token.
+} scanner_t;
+
+//Initiates scanner on scanner address.
+void scannerInit(scanner_t * scanner);
+//Free scanner on scanner address.
+void scannerFree(scanner_t * scanner);
+//Initiates token on token address.
+void tokenInit(token_t * token);
+//Returns token on token address to it's initial state.
+void tokenReinit(token_t * token);
+//Free token on token address.
+void tokenFree(token_t * token);
+//Reads next token from stdin with scanner on scanner address and returns pointer to new token.
+token_t * readToken(scanner_t * scanner);
 
 #endif
