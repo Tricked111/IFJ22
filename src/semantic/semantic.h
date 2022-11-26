a1a915cf74512f3e6f1b8a390417624933628d75
Nikita <nikita.kotvitski@gmail.com>
2022-11-26 20:51:24 +0100

    added function declaration control

--- src/semantic/semantic.h
+++ src/semantic/semantic.h
@@ -22,4 +22,20 @@ int varToken(const program_t *program, int poss, Symtable *table, token_t token)
 int funCallToken(const program_t *program, int poss, Symtable *table, token_t token);
 int semanticControl(const program_t *program);
 
+typedef enum {
+    SF_START,
+    SF_END,
+    SF_FUN_DEC,
+    SF_SKIP_BR,
+    SF_GET_PARAM,
+    SF_SKIP_NAME,
+    SF_CHECK_NEXT,
+    SF_SKIP_COLON,
+    SF_GET_TYPE,
+    SF_GET_QUEST_TYPE,
+    SF_FUN_START
+} FunDefsStates;
+
+int getFunTable(const program_t * program, Symtable * funcTable);
+
 #endif
