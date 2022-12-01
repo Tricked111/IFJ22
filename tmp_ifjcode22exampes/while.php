8e98bc89cf514103349503dd61b6631042b2376a
Nikita <nikita.kotvitski@gmail.com>
2022-12-01 16:55:14 +0100

    added while rules

---
+++ tmp_ifjcode22exampes/while.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+$a = 5;
+while ($a !== null) {
+    $a = $a - 1;
+    write($a);
+}
+write($a);
+
+$a = 3;
+$b = 4;
+
+while ($a !== null) {
+    while (b !== 1) {
+        $b = $b - 1;
+        write("|");
+    }
+    $a = $a - 1;
+}
+
+write($a);
+write("\n");
+write($b);
+write("\n");
